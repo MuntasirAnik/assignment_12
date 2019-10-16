1c3b74ac4d6c4e02a6594a2afd7455d9d30130c5
MuntasirAnik <anik.muntasir005@gmail.com>
2019-10-16 18:07:20 +0600

    assignment_12

---
+++ Assignment_11/Assignment_11/Refrigerator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_11
+{
+    public class Refrigerator
+    {
+        private double maxWeight { set; get; }
+        List<int> items = new List<int>();
+        List<double> weightPerUnit = new List<double>();
+
+
+        public Refrigerator(int max)
+        {
+            this.maxWeight = max;
+        }
+        public bool Add(int item, double weight)
+        {
+            bool isAdded = false;
+            this.items.Add(item);
+            this.weightPerUnit.Add(weight);
+            isAdded = true;
+            return isAdded;
+        }
+        public double Current()
+        {
+            double temp=0;
+            double currentWeight =0;
+            for(int i= 0; i<items.Count();i++)
+            {
+                temp = items[i] * weightPerUnit[i];
+                currentWeight += temp;
+
+            }
+            return currentWeight;
+        }
+        public bool Validation(double  addWeight)
+        {
+            bool isExeceeded = false;
+            if(Current()+addWeight >maxWeight)
+            {
+                isExeceeded = true;
+               
+            }
+            return isExeceeded;
+
+
+        }
+        public double RemainWeight()
+        {
+            return maxWeight - Current();
+        }
+        public string GetDetails()
+        {
+            string message;
+            message = "\tNo of item\tWeight.unit\tTotal Weight\n";
+            message += "--------------------------------------------------------------------------------------------\n";
+            List<double> tempList = new List<double>();
+            for (int i = 0; i < items.Count(); i++)
+            {
+                tempList.Add(this.items[i] * this.weightPerUnit[i]);
+                message += "\t" + items[i] + "\t\t" + weightPerUnit[i] + "\t\t" + tempList[i] + "\n";
+            }
+            message += "____________________________________________________\n";
+            message += "Total\t" + items.Sum() + "\t\t" + weightPerUnit.Sum() + "\t\t" + tempList.Sum();
+
+
+
+            return message;
+        }
+    }
+}
---
+++ Assignment_11/Assignment_11/RefrigeratorUi.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_11
+{
+    public partial class RefrigeratorUi : Form
+    {
+        Refrigerator refrigerator;
+        bool isCreated = false;
+        public RefrigeratorUi()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+           if(String.IsNullOrEmpty(maxWeightTextBox.Text))
+            {
+                MessageBox.Show("max weight must be an integer value");
+                return;
+            }
+                refrigerator = new Refrigerator(Convert.ToInt32(maxWeightTextBox.Text));
+                isCreated = true;
+                MessageBox.Show("Saved");
+           
+               
+            
+            
+        }
+
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            if(isCreated)
+            {
+                if(String.IsNullOrEmpty(itemsTextBox.Text)||String.IsNullOrEmpty(weightKgTextBox.Text))
+                {
+                    MessageBox.Show("item & weight must be integer");
+                    return;
+                }
+                double addWeight = 0;
+                addWeight = Convert.ToDouble(itemsTextBox.Text) * Convert.ToDouble(weightKgTextBox.Text);
+
+                if (refrigerator.Validation(addWeight))
+                {
+                    MessageBox.Show("Weight Execeeded");
+                }
+                else
+                {
+                    weightDetailRichTextBox.Clear();
+
+
+                    refrigerator.Add(Convert.ToInt32(itemsTextBox.Text), Convert.ToDouble(weightKgTextBox.Text));
+                    currentWeightTextBox.Text = Convert.ToString(refrigerator.Current());
+
+                    remainingWeightTextBox.Text = Convert.ToString(refrigerator.RemainWeight());
+                    weightDetailRichTextBox.Text= refrigerator.GetDetails();
+                    MessageBox.Show("Entered");
+
+
+                  //  weightTextBox.Clear();
+                  //  itemsTextBox.Clear();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Enter the max weight first");
+            }
+
+            
+           
+
+        }
+    }
+}
